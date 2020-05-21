699a7dd1787708b540369f39ea767f7a5e159efc
LucasWP03 <pedro.lucaswpl@gmail.com>
2020-05-21 14:07:48 -0300

    Aula 03 - Operações numéricas

---
+++ aula03.php
@@ -0,0 +1,23 @@
+//Pelo conteúdo ser muito básico, resolvi pular a aula em questão.
+<?php
+	$num1=10;
+	$num2=90;
+
+	//Aritméticos
+	$resultado=$num1+$num2;
+	echo "Soma de $num1+$num2 = $resultado<br>";
+	$resultado=$num2-$num1;
+	echo "Subtração de $num2-$num1 = $resultado<br>";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Aula 03</title>
+</head>
+<body>
+
+	
+</body>
+</html>
