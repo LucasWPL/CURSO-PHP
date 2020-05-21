012dd3bf7d939c4f565df473bebcf5ef29754684
LucasWP03 <pedro.lucaswpl@gmail.com>
2020-05-21 14:09:48 -0300

    Aula 08 - While

---
+++ aula07.php
@@ -0,0 +1,30 @@
+<?php
+	//WHILE
+	$i=0;
+	$tamanho=10;
+	$vet=array($tamanho);
+
+	while ($i <= $tamanho) {
+		echo "$i<br>";
+		$i++;
+	}
+	echo "<br><hr><br>";
+	$i=0;
+	while ($i <= $tamanho) {
+		$vet[$i] = ($i * 10);
+		echo "$vet[$i]<br>";
+		$i++;
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Aula 07</title>
+</head>
+<body>
+
+	
+</body>
+</html>
