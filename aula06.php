6d934f4e225232d354eb7a6374cf041e2d4cc470
LucasWP03 <pedro.lucaswpl@gmail.com>
2020-05-21 14:09:10 -0300

    Aula 06 - Switch e Case

---
+++ aula06.php
@@ -0,0 +1,34 @@
+<?php
+	//SWITCH CASE
+	$opc=1;
+
+	switch ($opc) {
+		case 1:
+			echo "Carro<br>";
+			break;
+		case 2:
+			echo "Moto<br>";
+			break;
+		case 3:
+			echo "Bicicleta<br>";
+			break;
+		case 4:
+			echo "Navio<br>";
+			break;
+		default:
+			echo "Inválido.";
+			break;
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Aula 06</title>
+</head>
+<body>
+
+	
+</body>
+</html>
