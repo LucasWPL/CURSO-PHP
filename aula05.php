52d18648c809b863e81dd909b07bf1e49c83a8f9
LucasWP03 <pedro.lucaswpl@gmail.com>
2020-05-21 14:08:34 -0300

    Aula 05 - If e Else

---
+++ aula05.php
@@ -0,0 +1,24 @@
+<?php
+	//IF e ELSE
+	$nota=6;
+
+	if ($nota >= 6) {
+		echo "<font color=green>Aprovado!</font>";
+	}else if($nota>=4 && $nota <6){
+		echo "<font color=yellow>Lista de espera!</font>";
+	}else{
+		echo "<font color=red>Reprovado!</font>";
+	};
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Aula 05</title>
+</head>
+<body>
+
+	
+</body>
+</html>
