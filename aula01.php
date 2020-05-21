f1afb2328ee54f732d4e2128af172037af7b46f9
LucasWP03 <pedro.lucaswpl@gmail.com>
2020-05-21 14:05:05 -0300

    Aula 01 - Echo

---
+++ aula01.php
@@ -0,0 +1,20 @@
+<?php
+
+	echo "<font color=red>Primeira Linha em PHP<br></font>";
+	echo "Curso de PHP";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Aula 01</title>
+</head>
+<body>
+
+	<p> Trecho escrito em HTML</p>
+	<?php 
+		echo "Trecho escrito em PHP";
+	?>
+</body>
+</html>
