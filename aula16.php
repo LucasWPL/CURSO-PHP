a1ded62de3e9efddffc498d44cfb3632b863160c
LucasWP03 <pedro.lucaswpl@gmail.com>
2020-05-23 21:48:36 -0300

    Aula 16 - Include

---
+++ aula16.php
@@ -0,0 +1,17 @@
+<?php
+    //Include
+   
+    include "data.inc";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Aula 16</title>
+</head>
+<body>
+
+	
+</body>
+</html>
