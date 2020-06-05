813e4ae922294f2b2dfa25e04bc9b70ddeb9dff6
LucasWP03 <pedro.lucaswpl@gmail.com>
2020-06-05 15:57:24 -0300

    Aula 28 - Comandos primários BDD

---
+++ aula28.php
@@ -0,0 +1,15 @@
+<?php
+
+    include "conexao.inc";
+    mysqli_close($con);
+    
+?>
+
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Aula 28</title>
+</head>
+<body>
+</body>
+</html>
