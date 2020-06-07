5ebfa34b452aec7a53e7ce119fddf87df3db25f2
LucasWP03 <pedro.lucaswpl@gmail.com>
2020-06-07 11:29:42 -0300

    Aula 33 - Select 2

--- aula33.php
+++ aula33.php
@@ -2,14 +2,19 @@
     // Select 
     include "conexao.inc";
     
+    //VERIFICANDO SE RECEBEU UM VALOR NA VCAT
     if(isset($_POST["f_cat"])){
         $vcat = $_POST["f_cat"];
     }else{
         $vcat = 0;
     }
+
+    //COMANDO PARA SELECIONAR TODOS OS ITENS DA CATEGORIA INFORMADA
     $sql = "SELECT * FROM tb_produtos WHERE cat = $vcat";
     $res = mysqli_query($con,$sql);
+    
     if ($vcat != 0){
+        //CRIAÇÃO DA TABELA
         echo "<table border='1' align='center'>";
         echo "<thead>".
         "<tr><th>Código</th>".
@@ -19,12 +24,14 @@
         "<th>Categoria</th>". 
         "</thead>";
         while($reg = mysqli_fetch_row($res)){
+            //VALORES DA TABELA
             $vcod = $reg[0];
             $vprod = $reg[1];
             $vpreco = $reg[2];
             $vestoque = $reg[3];
             $vcateg = $reg[4];
 
+            //LINHAS DA TABELA
             echo "<tr><td>$vcod</td>". 
             "<td>$vprod</td>". 
             "<td>$vpreco</td>". 
