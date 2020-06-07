83d6f1bda6abe7a6c0853e05811b572e2160ac6f
LucasWP03 <pedro.lucaswpl@gmail.com>
2020-06-07 11:27:28 -0300

    Aula 33 - Select 2

---
+++ aula33.php
@@ -0,0 +1,49 @@
+<?php
+    // Select 
+    include "conexao.inc";
+    
+    if(isset($_POST["f_cat"])){
+        $vcat = $_POST["f_cat"];
+    }else{
+        $vcat = 0;
+    }
+    $sql = "SELECT * FROM tb_produtos WHERE cat = $vcat";
+    $res = mysqli_query($con,$sql);
+    if ($vcat != 0){
+        echo "<table border='1' align='center'>";
+        echo "<thead>".
+        "<tr><th>Código</th>".
+        "<th>Produto</th>". 
+        "<th>Preço</th>". 
+        "<th>Estoque</th>". 
+        "<th>Categoria</th>". 
+        "</thead>";
+        while($reg = mysqli_fetch_row($res)){
+            $vcod = $reg[0];
+            $vprod = $reg[1];
+            $vpreco = $reg[2];
+            $vestoque = $reg[3];
+            $vcateg = $reg[4];
+
+            echo "<tr><td>$vcod</td>". 
+            "<td>$vprod</td>". 
+            "<td>$vpreco</td>". 
+            "<td>$vestoque</td>". 
+            "<td>$vcateg</td></tr>";
+            
+        }
+    }else{
+
+    }
+    mysqli_close($con);
+    
+?>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Aula 33</title>
+    </head>
+    <body>
+    </body>
+</html>
